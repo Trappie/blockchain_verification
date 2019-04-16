9daea63033e3684baa7172d3b13f82fb646d29f9
sphjlin <sphjlin@sina.cn>
2019-04-16 02:17:55 -0400

    Final version
    still need to work on the last test case

--- utils_test.rb
+++ utils_test.rb
@@ -41,18 +41,19 @@ class UtilsTest < Minitest::Test
     @account_set = SortedSet.new
   end
 
+
   # test the hash_str function
   # 1. test that the hash function work correctly when given a valid first line
   def test_hash_str_first_line
     str_to_hash = '0|0|SYSTEM>569274(100)|1553184699.650330000'
     assert_equal '288d', hash_str(str_to_hash, @encoding_array)
   end
-
   # 2. test that the hash function work correctly on a given line in the middle of the blockchain
   def test_hash_str_other_line
     str_to_hash = '1|288d|569274>735567(12):735567>561180(3):735567>689881(2):SYSTEM>532260(100)|1553184699.652449000'
     assert_equal '92a2', hash_str(str_to_hash, @encoding_array)
   end
+
 
   # test verify_user_input method
   # 1. test that verify_user_input method return [true, filename] when given a valid filename
@@ -68,6 +69,7 @@ class UtilsTest < Minitest::Test
     assert_equal [false, 'not_exist'], verify_user_input(['not_exist'])
   end
   
+
   # test read_file method
   # 1. test that read_file method read the given file and return the lines of the file in an array
   def test_read_file_success
@@ -77,6 +79,7 @@ class UtilsTest < Minitest::Test
   def test_read_file_empty
     assert_equal [], read_file('empty.data')
   end
+
 
   # test valid_hash? method (need stub hash_str method)
   # 1. test that valid_hash? method return true and remain silence when the given blocks array is valid
@@ -116,20 +119,21 @@ class UtilsTest < Minitest::Test
       block_array << line
     end
     accounts = []
-#    assert valid_blocks?(block_array, accounts)
-#    assert_output(""){valid_blocks?(block_array, accounts)}
+    assert valid_blocks?(block_array, accounts)
+    assert_output(""){valid_blocks?(block_array, accounts)}
   end
 
 
   # test verify method (need to stub read_file, valid_hash?, valid_blocks?)
   # 1. test that if the given blockchain is valid, output the balance of all accounts
-#  def test_verify_valid
-#    assert_output(""){verify('sample.data')}#
-#  end
-  # 2. test that if the given blockchain is invalid, output nothing
-#  def test_verify_invalid
-#    assert_output(""){verify('invalid_sample.data')}
-#  end
+  def test_verify_valid
+    assert_output("039873: 0 billcoins\n070668: 0 billcoins\n236340: 18 billcoins\n255185: 1 billcoins\n338967: 100 billcoins\n390411: 2 billcoins\n402207: 78 billcoins\n444100: 102 billcoins\n471345: 1 billcoins\n532260: 80 billcoins\n537882: 100 billcoins\n561180: 1 billcoins\n569274: 146 billcoins\n577469: 7 billcoins\n600381: 2 billcoins\n689881: 132 billcoins\n717802: 1 billcoins\n735567: 4 billcoins\n780971: 13 billcoins\n794343: 109 billcoins\n937639: 100 billcoins\n951990: 1 billcoins\n995917: 2 billcoins\n"){verify('sample.data')}
+  end
+  # 2. test that if the given blockchain is invalid, block by valid_blocks, print error message
+  def test_verify_invalid
+    assert_output("Line 1: Previous timestamp 1553184699.652449000 >= new timestamp 1553184699.650330000\nBLOCKCHAIN INVALID\n"){verify('invalid_sample.data')}
+  end
+
 
   # test greater? method
   # 1. test that greater? return true if time1 > time2
@@ -167,6 +171,7 @@ class UtilsTest < Minitest::Test
     refute greater?(time1, time2)
   end
 
+
   # test valid_transaction_pattern? method
   # 1. test that the method return true when given valid transaction with SYSTEM in front
   def test_valid_transaction_pattern_SYSTEM_in_front
@@ -177,7 +182,7 @@ class UtilsTest < Minitest::Test
     assert valid_transaction_pattern?('569274>735567(12)')
   end
   # 3. test that the method return false when given invalid transaction with SYSTEM in the second place
-  def test_valid_transaction_pattern_not_SYSTEM_front
+  def test_valid_transaction_pattern_SYSTEM_in_second
     refute valid_transaction_pattern?('569274>SYSTEM')
   end
   # 4. test that the method return false when the overall pattern is wrong
@@ -185,26 +190,34 @@ class UtilsTest < Minitest::Test
     refute valid_transaction_pattern?('lalalalalala')
   end
 
+
   # test extract_info method
   # 1. test that the method return the correct value when given a valid input
   def test_extract_info_valid
     assert_equal ['402207', 794343, 10], extract_info('402207>794343(10)')
   end
 
+
   # test apply_transaction method (need stub extract_info)
-  # 1. test that given a transactio with SYSTEM in front, the result is correct
-#  def test_apply_transaction_SYSTEM_front_result_correct
-#    assert_equal 1, apply_transaction('SYSTEM>532260(100)',[], @negative)
-#  end
+  # 1. test that given a transaction with SYSTEM in front, the result is correct
+  def test_apply_transaction_SYSTEM_front_result_correct
+    accounts = []
+    apply_transaction('SYSTEM>569274(100)', accounts, @negative)
+    assert_equal 100, accounts[569274]
+  end
   # 2. test that given a normal transaction without SYSTEM involved, the result is correct
-  # 3. test that after the transaction, if the first account's balance become negative, it will be in the negative_set
-  # 4. test that after the transaction, if the second account's balance is positive, it will be moved out of negative_set
-  # 5. test that given index that the value is 'nil', init it as 0 and calculate the result
-    def test_apply_transaction_value_is_0
+  def test_apply_transaction_noSYSTEM_result_correct
     accounts = []
     apply_transaction('402207>794343(10)', accounts, @negative)
     assert_equal 10, accounts[794343]
   end
+  # 3. test that given index that the value is 'nil', init it as 0 and calculate the result
+  def test_apply_transaction_value_is_0
+    accounts = []
+    apply_transaction('402207>794343(10)', accounts, @negative)
+    assert_equal 10, accounts[794343]
+  end
+
 
   # test verify_transactions method (stub valid_transaction_pattern?, apply_transaction, @negative)
   # 1. test that if the given transactions string is empty, return false and print error message
@@ -220,21 +233,19 @@ class UtilsTest < Minitest::Test
     refute verify_transactions(invalid_transactions, block_array, 0)
     assert_output("Line 0: Cannot parse transaction 'abc'\n"){verify_transactions(invalid_transactions, block_array, 0)}
   end
-  # 3. test that if all transactions are in valid pattern, the apply_transaction method will be called n times(n transaction)
-  def test_verify_transactions_pattern_wrong
+  # 3. test that if any account is negative after the transactions, return false and print error message
+  def test_verify_transactions_negative
     block_array = []
-    invalid_transactions = 'abc'
-    assert_equal verify_transactions(invalid_transactions, block_array, 0)
+    negative_transactions = 'SYSTEM>569274(-100)'
+    refute verify_transactions(negative_transactions, block_array, 0)
+    assert_output("Line 0: transaction list is empty\n"){verify_transactions('', block_array, 0)}
   end
-  # 4. test that if any account is negative after the transactions, return false and print error message
-
-  # 5. test that if the input is valid, print nothing and return true
+  # 4. test that if the input is valid, print nothing and return true
   def test_verify_transactions_all_valid
-    block_array = []
-    File.open('sample.data').each do |line|
-      block_array << line
-    end
-##################################
+    accounts = []
+    valid_transaction = 'SYSTEM>569274(100)'
+    assert verify_transactions(valid_transaction, accounts, 0)
+    assert_output(""){verify_transactions(valid_transaction, accounts, 0)}
   end
 
   # test show_error_message
@@ -249,17 +260,35 @@ class UtilsTest < Minitest::Test
     assert_output("Usage: ruby verifier.rb <name_of_file>\n       name_of_file = name of file to verify\n"){show_usage}
   end
 
+
   # test verify_line (stub greater?, verify_transactions)
   # 1. test that report error when the pattern of the line is wrong
-#  def test_verify_line_pattern_wrong
-#    assert_output ("Line 1: Cannot parse the block '1'"){verify_line(1, 0, '288d', 1553184699.650330000, 1553184699.652449000)}
-#  end
+  def test_verify_line_pattern_wrong
+    # let values.size != 5
+    assert_output ("Line 1: Cannot parse the block '1'\n"){verify_line("1", 0, '288d', '1553184699.650330000', @account_set)}
+  end
   # 2. test that report error when the line number is wrong
-#  def test_verify_line_lineNum_wrong
-#    assert_output ("Line 11111: Cannot parse the block '1'"){verify_line(11111, 0, '288d', 1553184699.650330000, 1553184699.652449000)}
-#  end
+  def test_verify_line_lineNum_wrong
+    # changed the 1 before the first '|' into 2
+    assert_output ("Line 1: Invalid block number 2, should be 1\n"){verify_line("2|288d|569274>735567(12):735567>561180(3):735567>689881(2):SYSTEM>532260(100)|1553184699.652449000|92a2\n", 0, '288d', '1553184699.650330000', @account_set)}
+  end
   # 3. test that report error when the hash is not consistant
+  def test_verify_line_hash_notConsistant
+    # changed the 288d before the second '|' into 292a
+    assert_output ("Line 1: Previous hash was 292a, should be 288d\n"){verify_line("1|292a|569274>735567(12):735567>561180(3):735567>689881(2):SYSTEM>532260(100)|1553184699.652449000|92a2\n", 0, '288d', '1553184699.650330000', @account_set)}
+  end
   # 4. test that report error when the timestamp is wrong
+  def test_verify_line_timestamp_wrong
+    # changed the time before the forth '|' into 1553184699.650330000
+    assert_output ("Line 1: Previous timestamp 1553184699.650330000 >= new timestamp 1553184699.650330000\n"){verify_line("1|288d|569274>735567(12):735567>561180(3):735567>689881(2):SYSTEM>532260(100)|1553184699.650330000|92a2\n", 0, '288d', '1553184699.650330000', @account_set)}
+  end  
   # 5. test that it return [false, nil, nil, nil] when the transactions are not valid
+  def test_verify_line_transactions_invalid
+    assert_equal [false, nil, nil, nil], verify_line("0|0|SYSTEM>569274(-100)|1553184699.650330000|288d\n", 0, '0', '0.0', @account_set)
+  end
   # 6. test that it return [true, line_number, curr_hash, time] when the line is valid
+  def test_verify_line_transactions_valid
+    assert_equal [true, 1, '292a', '1553184699.652449000'], verify_line("1|288d|569274>735567(12):735567>561180(3):735567>689881(2):SYSTEM>532260(100)|1553184699.652449000|92a2\n", 0, '288d', '1553184699.650330000', @account_set)
+#    assert_equal [true, 0, '288d', '1553184699.650330000'], verify_line("0|0|SYSTEM>569274(100)|1553184699.650330000|288d\n", 0, '0', '0.0', accounts)
+  end
 end
